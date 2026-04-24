ac0e8c9b76ebabff4ba8339ee387dc2debc98a97
Nika Bazon <tvoj_github_email@example.comgit>
2026-04-24 14:54:46 +0200

    Implement login screen with sign in and sign up options

--- app/src/main/java/com/example/f1fantasyleague/MainActivity.kt
+++ app/src/main/java/com/example/f1fantasyleague/MainActivity.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.f1fantasyleague.ui.theme.F1FantasyLeagueTheme
+import androidx.compose.runtime.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            F1FantasyLeagueTheme {
-                App()
+            setContent {
+                F1FantasyLeagueTheme {
+
+                    var isLoggedIn by remember { mutableStateOf(false) }
+
+                    if (isLoggedIn) {
+                        App() // ili HomeScreen()
+                    } else {
+                        LoginScreen(
+                            onLoginClick = {
+                                isLoggedIn = true
+                            }
+                        )
+                    }
+                }
             }
         }
     }
-}
